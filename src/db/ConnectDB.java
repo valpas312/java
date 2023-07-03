6a6e0041fe2e57b0df14deef77e7e41a8c1f24d7
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-07-03 16:00:54 -0300

    usando mysql y java

---
+++ src/db/ConnectDB.java
@@ -0,0 +1,36 @@
+package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectDB {
+    public static void main(String[] args) {
+
+        try {
+            // Establecer la conexión
+            String jdbcUrl = "jdbc:mysql://localhost:3306/dbescuela";
+            String usuario = "root";
+            String contraseña = "4166";
+            Connection connection = DriverManager.getConnection(jdbcUrl, usuario, contraseña);
+
+            // Realizar la consulta SELECT
+            String consulta = "SELECT * FROM alumnos";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(consulta);
+
+            // Procesar los resultados
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("nombres") + " " + resultSet.getString("apellidos"));
+            }
+
+
+            // Cerrar la conexión
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
