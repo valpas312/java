f0b5a79243edad75f6f7e300c484d7d9b3c48061
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-07-03 23:54:09 -0300

    iniciando crud

---
+++ src/crud/DatabaseConnector.java
@@ -0,0 +1,14 @@
+package crud;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/dbescuela";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "4166";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
